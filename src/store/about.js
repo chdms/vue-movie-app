2552c51947ac5663a89caa2322192277be1fc5b5
chdms <chdms267@naver.com>
2022-02-04 15:29:25 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+    namespaced :  true,
+    state : () => ({
+        name : 'HEROPY',
+        email : 'thescon@gamil.com',
+        blog : 'http://www.naver.com',
+        phone : '+82-10-1234-5678',
+        image : 'https://heropy.blog/css/images/logo.png'
+    })
+
+}
